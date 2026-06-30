2ddbc89cbc2ec0a275c58d3fccc0170b71de53bb
Emmes1026 <Maly1026@gmail.com>
2026-06-30 11:57:46 +0200

    feat(frontend): create page structure
    - Add basic layout architecture
    - Add shadcn tabs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
 
+
 function App() {
   return (
-    <div className="p-10 bg-slate-900 text-white min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to my app dashboard</h1>
-      <p className="mt-4">Django data will be here shortly</p>
+    <div className= "min-h-screen flex flex-col bg-red-100">
+
+      <div className= "flex bg-blue-100  p-6">
+        
+      </div>
+
+      <div className= "flex bg-green-200 p-6">
+
+      </div>
+      
+      <div className= "grow bg-slate-100 p-6">
+
+      </div>
+
+      <div className= "flex bg-sky-200 p-6">
+      </div>
+
     </div>
+
   )
 }
 export default App
