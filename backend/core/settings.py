e34fb48a17a9a14da0417eb66caa9015a4c5bcbc
Emmes1026 <Maly1026@gmail.com>
2026-07-07 10:35:09 +0200

    chore(backend): add frontend addresses to allowed CORS hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -132,7 +132,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 CORS_ALLOW_METHODS = (
